2dbb5b48e17ce2f4abd0bdecf3df96f60e3ba4a0
sina <3inroydl@gmail.com>
2024-01-09 21:51:40 +0330

    add screenshot

--- src/main/java/com/lzw/algorithm/HelloApplication.java
+++ src/main/java/com/lzw/algorithm/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Hello!");
+        stage.setTitle("LZMA ALGORITHM!");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
