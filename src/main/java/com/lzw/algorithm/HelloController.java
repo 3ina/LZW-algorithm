82dad05f5a455495e19c4db85fece0884a762b52
sina <3inroydl@gmail.com>
2024-01-10 13:53:38 +0330

    create fileTab

--- src/main/java/com/lzw/algorithm/HelloController.java
+++ src/main/java/com/lzw/algorithm/HelloController.java
@@ -3,24 +3,33 @@ package com.lzw.algorithm;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import com.lzw.algorithm.LZWCompression;
+import javafx.stage.FileChooser;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class HelloController {
     @FXML
-    private Button compress_btn,decompress_btn;
+    private Button compress_btn,decompress_btn,selectFileBtn,saveBtnFile;
+    @FXML
+    private SplitPane splitPane;
     @FXML
     private TabPane mainTab;
+    @FXML
+    private Label fileSelectedLabel;
     @FXML
     private Tab textInputTab,fileTab,ImageTab;
     @FXML
     private AnchorPane mainAnchorPane;
     @FXML
     private TextArea inputTextArea,outputTextArea;
+
+    private String filePath = null;
 
     @FXML
     protected void compressButtonAction(ActionEvent event) {
@@ -35,6 +44,21 @@ public class HelloController {
 
             }
             outputTextArea.setText(resultText);
+        }else if(mainTab.getSelectionModel().getSelectedItem().getId() == fileTab.getId()){
+            if(filePath != null){
+
+            }else {
+                Dialog<String> dialog = new Dialog<String>();
+                dialog.setTitle("Dialog");
+                ButtonType type = new ButtonType("Ok", ButtonBar.ButtonData.OK_DONE);
+                //Setting the content of the dialog
+                dialog.setContentText("Please first select your file");
+                //Adding buttons to the dialog pane
+                dialog.getDialogPane().getButtonTypes().add(type);
+
+                dialog.showAndWait();
+
+            }
         }
 
     }
@@ -53,7 +77,36 @@ public class HelloController {
 
             String decodedString = LZWCompression.decode(encodedList);
             outputTextArea.setText(decodedString);
+        }else if(mainTab.getSelectionModel().getSelectedItem().getId() == fileTab.getId()) {
+            if (filePath != null) {
+
+            } else {
+                Dialog<String> dialog = new Dialog<String>();
+                dialog.setTitle("Dialog");
+                ButtonType type = new ButtonType("Ok", ButtonBar.ButtonData.OK_DONE);
+                //Setting the content of the dialog
+                dialog.setContentText("Please first select your file");
+                //Adding buttons to the dialog pane
+                dialog.getDialogPane().getButtonTypes().add(type);
+
+                dialog.showAndWait();
+
+            }
         }
+    }
+
+    @FXML
+    protected void selectBtnFileAction(ActionEvent event){
+        FileChooser fileChooser = new FileChooser();
+        File selectedFile = fileChooser.showOpenDialog(splitPane.getScene().getWindow());
+        filePath = selectedFile.getPath();
+        fileSelectedLabel.setText("File path : "+filePath);
+
+
+    }
+
+    @FXML
+    protected void saveBtnFileAction(ActionEvent event){
 
     }
 
